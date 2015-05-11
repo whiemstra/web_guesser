addacd6fcd1040058e7f8aea3b25558319637c35
Whitney Hiemstra <whitcolorado@gmail.com>
2015-05-11 16:34:07 -0600

    I3: number guessing game complete

--- wed_guesser.rb
+++ wed_guesser.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess, number)
+  if guess == number
+    "You got it right!"
+  elsif guess > number && (guess - number) > 5
+    return "Way too high!"
+  elsif guess > number
+    return "Too High!"
+  elsif guess < number && (number - guess) > 5
+    return "Way too low!"
+  else
+    return "Too low!"
+  end
+end
+
 
 get '/' do
-  erb :index, :locals => {:number => number }
+  guess = params['guess'].to_i
+  message = check_guess(guess, SECRET_NUMBER)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message }
 end
